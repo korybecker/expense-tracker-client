4b376f146238f4a429a35d0a5550b0ec058b5aa3
Kory <korymbecker@gmail.com>
2022-01-15 13:00:23 -0500

    created each endpoint, each tested on postman

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const app = express();
 const expenses = require('./routes/expenses');
 
 
+
 // middleware
 app.use(express.json());
 
-//routes
+// routes
 app.use('/api/v1/expenses', expenses);
+
+// app.get('/api/v1/expenses');           - get all tasks
+// app.post('/api/v1/expenses');          - create new task
+// app.get('/api/v1/expenses/:id');       - get single task
+// app.patch('/api/v1/expenses/:id');     - update single task
+// app.delete('/api/v1/expenses/:id');    - delete single task
+
 
 // port
 const PORT = 3000 || process.env.PORT;
