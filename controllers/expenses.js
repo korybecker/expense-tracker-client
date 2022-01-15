5e7ee949da945da71c71312d4d7d500862408280
Kory <korymbecker@gmail.com>
2022-01-15 15:50:50 -0500

    updated controllers and expense schema, and created index and expense html pages

--- controllers/expenses.js
+++ controllers/expenses.js
@@ -1,26 +1,55 @@
+const res = require('express/lib/response');
+const expense = require('../models/expense');
 const Expense = require('../models/expense');
 
-const getAllExpenses = (req, res) => {
-    res.send('get all expenses');
+const getAllExpenses = async (req, res) => {
+    try {
+        const tasks = await Expense.find({});
+        res.status(200).json({tasks});
+    } catch(e) {
+        res.status(500).json({ msg: e });
+    }
 };
 
+// await expense creation (connects to database)
 const createExpense = async (req, res) => {
-    const expense = await Expense.create(req.body)
-    res.status(201).json({expense});
+    try {
+        const expense = await Expense.create(req.body);
+        res.status(201).json({ expense });         // 201 successful post
+    } catch (e) {
+        res.status(500).json({ msg: e });
+    }
 };
 
-const getSingleExpense = (req, res) => {
-    res.json({id:req.params.id});
+const getSingleExpense = async (req, res) => {
+    try {
+        const expense = await Expense.findById(req.params.id);
+        if (!expense) {
+            return res.status(404).json({ msg: `No task with id: ${req.params.id}`});
+        }
+        res.status(200).json({ expense });
+    } catch (e) {
+        res.status(500).json({ msg: e });
+    }
 };
 
-const updateExpense = (req, res) => {
-    res.send('update expense');
+const updateExpense = async (req, res) => {
+    
 };
 
-const deleteExpense = (req, res) => {
-    res.send('delete expense');
+const deleteExpense = async (req, res) => {
+    try {
+        const expense = await Expense.findByIdAndRemove(req.params.id);
+        if (!expense) {
+            return res.status(404).json({ msg: `No task with id ${req.params.id}`});
+        }
+        res.status(200).json({ expense })
+    } catch (e) {
+        res.status(500).json({ msg: e })
+    }
 };
 
+// export all controller middlewares (used in routes)
 module.exports = {
     getAllExpenses,
     createExpense,
