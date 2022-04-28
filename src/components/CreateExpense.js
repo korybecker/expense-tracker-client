54f7aa2f88bfb6d4c8fda3e6bc753fd42952dcc0
Kory <korymbecker@gmail.com>
2022-04-28 16:46:31 -0400

    updated navigate route to / instead of /expenses

--- src/components/CreateExpense.js
+++ src/components/CreateExpense.js
@@ -29,7 +29,7 @@ const CreateExpense = () => {
         recurring,
       })
       .then((res) => {
-        navigate("/expenses");
+        navigate("/");
       })
       .catch((e) => {
         console.error(e);
